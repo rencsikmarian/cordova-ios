85adba51905e9d727ebd5a2719b82593ecaefafa
Rencsik Marian <mariann4you@gmail.com>
2024-04-25 16:13:36 +0300

    Update CDVPlugin.m V1

--- CordovaLib/Classes/Public/CDVPlugin.m
+++ CordovaLib/Classes/Public/CDVPlugin.m
@@ -102,6 +102,9 @@ NSString* const CDVViewWillTransitionToSizeNotification = @"CDVViewWillTransitio
     // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(viewDidLayoutSubviews:) name:CDVViewDidLayoutSubviewsNotification object:nil];
     // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(viewWillTransitionToSize:) name:CDVViewWillTransitionToSizeNotification object:nil];
 }
+- (void)apply_stuff_to_configuration: (id) configuration{
+
+}
 
 - (void)dispose
 {
